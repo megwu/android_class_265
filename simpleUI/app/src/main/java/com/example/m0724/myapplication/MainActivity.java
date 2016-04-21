6d9a3c1b121a7c756ce15df1b8d9cad31b353461
megwu <m07240629@gmail.com>
2016-04-21 19:57:15 +0800

    add button and edittext

--- simpleUI/app/src/main/java/com/example/m0724/myapplication/MainActivity.java
+++ simpleUI/app/src/main/java/com/example/m0724/myapplication/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.m0724.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView; //宣告 變數型態 變數名稱
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView); //byId找View物件,View是高級的型態,所以要轉型成TextView
+
+    }
+
+    public void click(View view) { //建立function 若要Import就按Alt + Enter ,然後再button的onClick下拉選單可以選到此function
+        textView.setText("Android Class"); //程式碼設定顯示的文字
     }
 }
