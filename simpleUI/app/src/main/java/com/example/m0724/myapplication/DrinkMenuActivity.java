06a2e06e83af9184fbaf52feba20470561d88163
megwu <m07240629@gmail.com>
2016-05-02 21:02:09 +0800

    implement getData and JSONArray

--- simpleUI/app/src/main/java/com/example/m0724/myapplication/DrinkMenuActivity.java
+++ simpleUI/app/src/main/java/com/example/m0724/myapplication/DrinkMenuActivity.java
@@ -3,6 +3,14 @@ package com.example.m0724.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class DrinkMenuActivity extends AppCompatActivity {
 
@@ -11,6 +19,60 @@ public class DrinkMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_menu);
         Log.d("debug", "DrinkMenuActivity OnCreate");
+    }
+
+    public void add(View view) {
+        Button button = (Button)view;
+
+        String text = button.getText().toString();
+
+        int count = Integer.parseInt(text);
+
+        count++;
+
+        button.setText(String.valueOf(count));
+    }
+
+    public void cancel(View view) {
+        finish(); // 直接 onDestroy 跳回前面那一頁
+    }
+
+    public void done(View view) {
+
+    }
+
+    public JSONArray getData() {
+        //JSON 就像shareReference  有key跟value
+
+        LinearLayout rootLinearLayout = (LinearLayout) findViewById(R.id.root);
+
+        JSONArray jsonArray = new JSONArray();
+
+        for (int i = 1; i < 4; i++) {
+            LinearLayout linearLayout = (LinearLayout) rootLinearLayout.getChildAt(i);
+
+            TextView textView = (TextView)linearLayout.getChildAt(0);
+            Button mButton = (Button)linearLayout.getChildAt(1);
+            Button lButton = (Button)linearLayout.getChildAt(2);
+
+            String drinkName = textView.getText().toString();
+
+            int m = Integer.parseInt(mButton.getText().toString());
+            int l = Integer.parseInt(lButton.getText().toString());
+
+            JSONObject jsonObject = new JSONObject();
+            try {
+                jsonObject.put("name", drinkName);
+                jsonObject.put("m", m);
+                jsonObject.put("l", l);
+
+                jsonArray.put(jsonObject);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return jsonArray;
     }
 
     @Override
